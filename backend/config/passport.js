1882e30e58648b102630056d7de16814eb464417
sravani-2005-coder <darsisravani2@gmail.com>
2026-08-02 12:01:46 +0530

    vrcel

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -3,14 +3,18 @@ import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import User from "../models/User.js";
 import dotenv from "dotenv";
 dotenv.config();
-console.log("GOOGLE_CALLBACK_URL =", process.env.GOOGLE_CALLBACK_URL);
+import dotenv from "dotenv";
+dotenv.config();
+
+console.log("GOOGLE_CLIENT_ID:", process.env.GOOGLE_CLIENT_ID);
+console.log("GOOGLE_CALLBACK_URL:", process.env.GOOGLE_CALLBACK_URL);
+
 passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: process.env.GOOGLE_CALLBACK_URL,
-    },
+  new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+  },
     async (accessToken, refreshToken, profile, done) => {
       try {
         let user = await User.findOne({
