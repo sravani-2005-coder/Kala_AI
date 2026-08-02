bd0a5cb4c027b6c16c754cf5220cda57c417d26e
sravani-2005-coder <darsisravani2@gmail.com>
2026-08-02 10:37:14 +0530

    frontend deploy

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ const app = express();
 app.use(
   cors({
     origin:["http://localhost:5173",
-    "https://kala-ai-fhj7.onrender.com"], 
+    "https://kala-ai-frontend-lake.vercel.app"], 
     credentials: true,
   })
 );
